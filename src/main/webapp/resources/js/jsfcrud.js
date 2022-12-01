60e6000f140bc1eda3c871e8c86644d117b692ab
connor3015 <connor3015@vt.edu>
2022-11-30 23:13:21 -0500

    Created blank pages for route crud and view

--- src/main/webapp/resources/js/jsfcrud.js
+++ src/main/webapp/resources/js/jsfcrud.js
@@ -1,3 +1,8 @@
+/*
+ * Created by Rob Guldi on 2022.11.30
+ * Copyright © 2022 Robert Guldi. All rights reserved.
+ */
+
 function handleSubmit(args, dialog) {
     var jqDialog = jQuery('#' + dialog);
     if (args.validationFailed) {
